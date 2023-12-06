a6fcdd48562ad58555c878354c0d48751ecefb59
zpuckeridge <zacchary@puckeridge.me>
2023-12-06 07:33:16 +0000

    chore: updated packages, added max-w to navbar

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -16,47 +16,49 @@ export default async function Navbar() {
 
   return (
     <nav className="sticky top-0 z-50 flex w-full items-center justify-between border-b-2 bg-neutral-900 p-4 text-white  lg:px-6">
-      <div className="mr-4 block flex-none md:hidden">
-        <MobileMenu menu={cultureMenu} />
-      </div>
-      <div className="flex w-full items-center">
-        <div className="flex w-full gap-4 md:w-1/3">
-          <div className="my-auto">
-            <Link className="flex font-bold" href={'/'}>
-              <Image
-                src="/logos/the-armoury-bookshop.png"
-                height="40"
-                width="40"
-                alt="The Armoury Bookshop Logo"
-                className="mr-4"
-              />
-              <div className="my-auto hidden transition-all duration-200 hover:text-muted-foreground md:block">
-                <p className="relative my-auto whitespace-nowrap font-logo text-xl leading-none">
-                  <span className="absolute -top-2.5 left-4 text-sm">The</span>
-                  Armoury
-                </p>
-                <p className="ml-2 whitespace-nowrap font-logo text-xl leading-none">Bookshop</p>
-              </div>
-            </Link>
-          </div>
+      <div className="mx-auto flex w-full max-w-7xl items-center justify-between">
+        <div className="mr-4 block flex-none md:hidden">
+          <MobileMenu menu={cultureMenu} />
         </div>
-        <div className="hidden justify-center md:flex md:w-1/3">
-          <MainNavigationMenu
-            cultureMenu={cultureMenu}
-            theologyMenu={theologyMenu}
-            kidsMenu={kidsMenu}
-          />
-        </div>
-        <div className="flex justify-end gap-2 md:w-1/3">
-          <div className="hidden lg:flex">
-            <Search />
+        <div className="flex w-full items-center">
+          <div className="flex w-full gap-4 md:w-1/3">
+            <div className="my-auto">
+              <Link className="flex font-bold" href={'/'}>
+                <Image
+                  src="/logos/the-armoury-bookshop.png"
+                  height="40"
+                  width="40"
+                  alt="The Armoury Bookshop Logo"
+                  className="mr-4"
+                />
+                <div className="my-auto hidden transition-all duration-200 hover:text-muted-foreground md:block">
+                  <p className="relative my-auto whitespace-nowrap font-logo text-xl leading-none">
+                    <span className="absolute -top-2.5 left-4 text-sm">The</span>
+                    Armoury
+                  </p>
+                  <p className="ml-2 whitespace-nowrap font-logo text-xl leading-none">Bookshop</p>
+                </div>
+              </Link>
+            </div>
           </div>
+          <div className="hidden justify-center md:flex md:w-1/3">
+            <MainNavigationMenu
+              cultureMenu={cultureMenu}
+              theologyMenu={theologyMenu}
+              kidsMenu={kidsMenu}
+            />
+          </div>
+          <div className="flex justify-end gap-2 md:w-1/3">
+            <div className="hidden lg:flex">
+              <Search />
+            </div>
 
-          <ModeToggle />
+            <ModeToggle />
 
-          <Suspense fallback={<OpenCart />}>
-            <Cart />
-          </Suspense>
+            <Suspense fallback={<OpenCart />}>
+              <Cart />
+            </Suspense>
+          </div>
         </div>
       </div>
     </nav>
--- components/oaks.tsx
+++ components/oaks.tsx
@@ -1,35 +1,37 @@
 export default function Oaks() {
   return (
-    <div className="flex w-full justify-between bg-black px-4 py-2">
-      <div className="scrollbar-hide inline-flex gap-8 overflow-auto whitespace-nowrap text-sm tracking-tight text-muted-foreground ">
-        <a
-          href="https://61oaksgroup.com.au"
-          target="_blank"
-          className="underline-offset-4 hover:text-white hover:underline"
-        >
-          61 Oaks Group
-        </a>
-        <a
-          href="https://haddoninstitute.org"
-          target="_blank"
-          className="underline-offset-4 hover:text-white hover:underline"
-        >
-          Haddon Institute
-        </a>
-        <a
-          href="https://starcompass.com.au"
-          target="_blank"
-          className="underline-offset-4 hover:text-white hover:underline"
-        >
-          Star Compass
-        </a>
-        <a
-          href="https://lbnuconsulting.com.au"
-          target="_blank"
-          className="underline-offset-4 hover:text-white hover:underline"
-        >
-          LBNU Consulting
-        </a>
+    <div className=" bg-black px-4 py-2">
+      <div className="mx-auto flex w-full max-w-7xl justify-between">
+        <div className="scrollbar-hide inline-flex gap-8 overflow-auto whitespace-nowrap text-sm tracking-tight text-muted-foreground ">
+          <a
+            href="https://61oaksgroup.com.au"
+            target="_blank"
+            className="underline-offset-4 hover:text-white hover:underline"
+          >
+            61 Oaks Group
+          </a>
+          <a
+            href="https://haddoninstitute.org"
+            target="_blank"
+            className="underline-offset-4 hover:text-white hover:underline"
+          >
+            Haddon Institute
+          </a>
+          <a
+            href="https://starcompass.com.au"
+            target="_blank"
+            className="underline-offset-4 hover:text-white hover:underline"
+          >
+            Star Compass
+          </a>
+          <a
+            href="https://lbnuconsulting.com.au"
+            target="_blank"
+            className="underline-offset-4 hover:text-white hover:underline"
+          >
+            LBNU Consulting
+          </a>
+        </div>
       </div>
     </div>
   );
